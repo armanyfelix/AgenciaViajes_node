1c65cc790a383f43468fa9a9962cb256426a88c0
armanyfelix <armanyfelix@outlook.com>
2021-09-23 15:12:39 -0700

    downgrade mysql

--- agenciaviajes.sql
+++ agenciaviajes.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.35, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: agenciaviajes
+-- Host: localhost    Database: agenciaviajes
 -- ------------------------------------------------------
--- Server version	8.0.25
+-- Server version	5.7.35-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,13 +21,13 @@
 
 DROP TABLE IF EXISTS `testimoniales`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `testimoniales` (
-  `id` int DEFAULT NULL,
+  `id` int(11) DEFAULT NULL,
   `nombre` varchar(60) DEFAULT NULL,
   `correo` varchar(60) DEFAULT NULL,
   `mensaje` text
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,19 +46,19 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `viajes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `viajes` (
-  `id` bigint NOT NULL,
+  `id` bigint(20) NOT NULL,
   `titulo` varchar(60) DEFAULT NULL,
   `precio` varchar(10) DEFAULT NULL,
   `fecha_ida` date DEFAULT NULL,
   `fecha_vuelta` date DEFAULT NULL,
   `imagen` varchar(15) DEFAULT NULL,
   `descripcion` text,
-  `disponibles` int DEFAULT NULL,
+  `disponibles` int(11) DEFAULT NULL,
   `slug` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -80,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-09-10 19:36:05
+-- Dump completed on 2021-09-23 14:15:40
